facb60f771538965cf543610d26e9010bc80361c
王西迪 <wxd237@gmail.com>
2017-03-16 00:11:59 +0800

    Create wxslab.h

---
+++ wxslab.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+extern size_t wxslab_pagesize;
+
+struct wxslab_header {
+    struct slab_header *prev, *next;
+    uint64_t slots;
+    uintptr_t refcount;
+    struct wxslab_header *page;
+    uint8_t data[] __attribute__((aligned(sizeof(void *))));
+};
+
+struct wxslab_chain {
+    size_t itemsize, itemcount;
+    size_t slabsize, pages_per_alloc;
+    uint64_t initial_slotmask, empty_slotmask;
+    uintptr_t alignment_mask;
+    struct slab_header *partial, *empty, *full;
+};
+
+void wxslab_init(struct wxslab_chain *, size_t);
+void *wxslab_alloc(struct wxslab_chain *);
+void wxslab_free(struct wxslab_chain *, const void *);
+void wxslab_traverse(const struct wxslab_chain *, void (*)(const void *));
+void wxslab_destroy(const struct wxslab_chain *);
